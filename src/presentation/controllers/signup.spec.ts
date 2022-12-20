90e1eab1eb3b832f9aec5b753e781b6f6de288bc
Mateus Sebastião Juvencio <32066370+Mateussj@users.noreply.github.com>
2022-12-20 08:20:22 -0300

    test: garantindo que o signupcontroller retorne erro se o email nao for enviado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,3 +16,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'teste_name',
+        password: 'teste_password',
+        passawordConfirmation: 'teste_passaword'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})

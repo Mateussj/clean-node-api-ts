55813b4096af60b24407e47b1fd0ed1adf4d125f
Mateus Juvencio <mateus.juvencio@axxindata.com>
2023-05-11 10:41:00 -0300

    test: garantindo que o SignupController retorne 200 para dados validos inseridos

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -14,13 +14,13 @@ const makeEmailValidator = (): EmailValidator => {
 const makeAddAccount = (): AddAccount => {
   class AddAcountStub implements AddAccount {
     add (account: AddAccountModel): AccountModel {
-      const faleAccount = {
+      const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email',
         password: 'valid_password'
       }
-      return faleAccount
+      return fakeAccount
     }
   }
   return new AddAcountStub()
@@ -208,5 +208,26 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_teste@email.com',
+        password: 'valid_teste_password',
+        passawordConfirmation: 'valid_teste_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    })
   })
 })
